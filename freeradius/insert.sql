a9eac16f3b74b1febe5a4b19c687ce9ca066cc09
HapyM3al <tianholmes58@gmail.com>
2020-01-04 18:31:46 +0200

    Create insert.sql

---
+++ freeradius/insert.sql
@@ -0,0 +1,12 @@
+# some inserts. 
+
+# this is fine for login to router and the varvalue needs to be whatever group name in /user/group
+
+INSERT INTO `radreply` VALUES (1,'username','Mikrotik-Group',':=','full');
+
+# add new username, winbox works with chap and ssh something else however for both to work need cleartext-password yay mikrotik.
+
+INSERT INTO `radcheck` VALUES (1,'username','Cleartext-Password',':=','password');
+
+# https://wiki.mikrotik.com/wiki/Manual:RADIUS_Client#Supported_RADIUS_Attributes
+# you can make data allocations and so on, ill add this at a later stage. 
