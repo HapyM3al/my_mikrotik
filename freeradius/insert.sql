6609801fb4af1eeca16fa8af8d7fed11bcf1a75b
HapyM3al <tianholmes58@gmail.com>
2020-01-04 18:42:23 +0200

    Update insert.sql

--- freeradius/insert.sql
+++ freeradius/insert.sql
@@ -8,6 +8,10 @@ INSERT INTO `radreply` VALUES (1,'username','Mikrotik-Group',':=','full');
 
 INSERT INTO `radcheck` VALUES (1,'username','Cleartext-Password',':=','password');
 
+# add nas
+
+INSERT INTO `nas` VALUES (1,'10.50.0.1','name','other',0,'secret',NULL,NULL,'desc'),(2,'127.0.0.1','testing123','other',0,'testing123',NULL,NULL,'localhost'),(3,'10.50.0.20','name','other',0,'ruckustesting',NULL,NULL,'ruckus_ap');
+
 # the above actually also works for 802.1X auth. brilliant I know. 
 
 # https://wiki.mikrotik.com/wiki/Manual:RADIUS_Client#Supported_RADIUS_Attributes
