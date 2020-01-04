1812c9f199df5243d8b173035c23816aea833f3d
HapyM3al <tianholmes58@gmail.com>
2020-01-04 18:46:39 +0200

    Update insert.sql

--- freeradius/insert.sql
+++ freeradius/insert.sql
@@ -2,7 +2,9 @@
 
 # this is fine for login to router and the varvalue needs to be whatever group name in /user/group
 
-INSERT INTO `radreply` VALUES (1,'username','Mikrotik-Group',':=','full');
+dump: INSERT INTO `radreply` VALUES (1,'username','Mikrotik-Group',':=','full');
+
+insert into radreply (username,attribute,op,value) VALUES ('usenrame','Mikrotik-Group',':=','full') ;
 
 # add new username, winbox works with chap and ssh something else however for both to work need cleartext-password yay mikrotik.
 
@@ -10,7 +12,10 @@ INSERT INTO `radcheck` VALUES (1,'username','Cleartext-Password',':=','password'
 
 # add nas
 
-INSERT INTO `nas` VALUES (1,'10.50.0.1','name','other',0,'secret',NULL,NULL,'desc'),(2,'127.0.0.1','testing123','other',0,'testing123',NULL,NULL,'localhost'),(3,'10.50.0.20','name','other',0,'ruckustesting',NULL,NULL,'ruckus_ap');
+dump: INSERT INTO `nas` VALUES (1,'10.50.0.1','name','other',0,'secret',NULL,NULL,'desc'),(2,'127.0.0.1','testing123','other',0,'testing123',NULL,NULL,'localhost'),(3,'10.50.0.20','name','other',0,'ruckustesting',NULL,NULL,'ruckus_ap');
+
+INSERT INTO nas (nasname,shortname,type,ports,secret,server,community,description) values ('10.50.0.20','name','other',0,'ruckustesting',NULL,NULL,'ruckus_ap')
+INSERT INTO nas (nasname,shortname,type,ports,secret,server,community,description) VALUES ('10.50.0.1','name','other','0','secret',NULL,NULL,'desc');
 
 # the above actually also works for 802.1X auth. brilliant I know. 
 
